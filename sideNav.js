bdd782f56f893ff05f8d581d040ed931f612eed9
Swmar23 <91944872+Swmar23@users.noreply.github.com>
2023-04-19 23:29:30 +0200

    Menu dla małych ekranów bez JavaScripta.

--- sideNav.js
+++ sideNav.js
@@ -5,3 +5,8 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+// Check if JavaScript is enabled
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("myBody").classList.add("js-enabled");
+});
