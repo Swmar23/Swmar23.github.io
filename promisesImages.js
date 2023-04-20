f3259d6821749f60687b4bc650fac8b203d8d1c8
Swmar23 <91944872+Swmar23@users.noreply.github.com>
2023-04-21 00:28:49 +0200

    Some corrections for JSLint.

--- promisesImages.js
+++ promisesImages.js
@@ -1,8 +1,10 @@
+ /*jslint devel: true */
+
 function loadIMG(url, id){
   //await new Promise(r => setTimeout(r, 1500));
   var P = new Promise( function (resolve, reject) {
       var parent = document.getElementById(id);
-      var element = document.createElement('img');
+      var element = document.createElement("img");
       element.setAttribute("src", url);
       element.setAttribute("alt", url);
       parent.appendChild(element);
@@ -14,38 +16,38 @@ function loadIMG(url, id){
 }
 
 Promise.all([
-    loadIMG('programming_logos/java.png','lang1'),
-    loadIMG('programming_logos/julia.png','lang1'), 
-    loadIMG('programming_logos/c++.png','lang1'), 
-    loadIMG('programming_logos/python.png','lang1'), 
-    loadIMG('programming_logos/mysql.png','lang1'),  
+    loadIMG("programming_logos/java.png","lang1"),
+    loadIMG("programming_logos/julia.png","lang1"), 
+    loadIMG("programming_logos/c++.png","lang1"), 
+    loadIMG("programming_logos/python.png","lang1"), 
+    loadIMG("programming_logos/mysql.png","lang1"),  
   ]).then(function() {
-    console.log('Wszystkie loga (lang1) załadowane poprawnie!');
+    console.log("Wszystkie loga (lang1) załadowane poprawnie!");
   }).catch(function() {
-    console.log('Błąd ładowania galerii rownoległej');
+    console.log("Błąd ładowania galerii rownoległej");
   });
 
 
 Promise.all([
-  loadIMG('programming_logos/c.png','lang2'),
-  loadIMG('programming_logos/html5.png','lang2'), 
-  loadIMG('programming_logos/css.png','lang2'), 
-  loadIMG('programming_logos/sass.png','lang2'), 
-  loadIMG('programming_logos/javascript.png','lang2'),  
-  loadIMG('programming_logos/swipl.png','lang2'),  
-  loadIMG('programming_logos/haskell.png','lang2'),  
-  loadIMG('programming_logos/kotlin.png','lang2'),  
+  loadIMG("programming_logos/c.png","lang2"),
+  loadIMG("programming_logos/html5.png","lang2"), 
+  loadIMG("programming_logos/css.png","lang2"), 
+  loadIMG("programming_logos/sass.png","lang2"), 
+  loadIMG("programming_logos/javascript.png","lang2"),  
+  loadIMG("programming_logos/swipl.png","lang2"),  
+  loadIMG("programming_logos/haskell.png","lang2"),  
+  loadIMG("programming_logos/kotlin.png","lang2"),  
 ]).then(function() {
-  console.log('Wszystkie loga (lang2) załadowane poprawnie!');
+  console.log("Wszystkie loga (lang2) załadowane poprawnie!");
 }).catch(function() {
-  console.log('Błąd ładowania galerii rownoległej');
+  console.log("Błąd ładowania galerii rownoległej");
 });
 
 Promise.all([
-  loadIMG('programming_logos/svn.png','lang3'),
-  loadIMG('programming_logos/git.png','lang3'), 
+  loadIMG("programming_logos/svn.png","lang3"),
+  loadIMG("programming_logos/git.png","lang3"), 
 ]).then(function() {
-  console.log('Wszystkie loga (lang3) załadowane poprawnie!');
+  console.log("Wszystkie loga (lang3) załadowane poprawnie!");
 }).catch(function() {
-  console.log('Błąd ładowania galerii rownoległej');
+  console.log("Błąd ładowania galerii rownoległej");
 });
